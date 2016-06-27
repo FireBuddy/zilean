e502c89107c666120b20497a5f761a649342d4a1
FireBuddy <aijun@live.ca>
2016-06-27 11:52:32 -0400

    Update Brain.cs

--- SwagZilean/SwagZilean/Brain.cs
+++ SwagZilean/SwagZilean/Brain.cs
@@ -72,6 +72,7 @@ namespace SwagZilean
                 
                 {
                  Spells.Q.Cast(CurrentTarget.ServerPosition + 20);
+                 Core.DelayAction(Spells.Q.Cast(sender), 150);
                 }
 
             }
