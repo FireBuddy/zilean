186c0cbf87625a265db26d09e5f2ba6d8cb51691
FireBuddy <aijun@live.ca>
2016-06-27 12:49:28 -0400

    Update Brain.cs

--- SwagZilean/SwagZilean/Brain.cs
+++ SwagZilean/SwagZilean/Brain.cs
@@ -82,7 +82,7 @@ namespace SwagZilean
                 {
                  var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Both,sender.ServerPosition, 150);
                  foreach (var Minion in Minions)
-                 if(_Player.Distance(Minion.ServerPosition) =< 900)
+                 if(_Player.Instance.Distance(Minion.ServerPosition) =< 900)
                  {
                     Spells.Q.Cast(Minion.ServerPosition);
                     Core.DelayAction( () => Spells.Q.Cast(Minion.ServerPosition), 500);
